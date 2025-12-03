1750bc88f6250fe6bd7950d86337dd19d1c78b88
Alazar42 <alazartesfaye42@gmail.com>
2025-12-03 13:08:37 +0300

    reorderd the game

--- games.py
+++ games.py
@@ -1,10 +1,5 @@
 games = [
     {
-        "name": "Match Africa",
-        "short_name": "matchafrica",
-        "url": "https://match-africa-host.vercel.app"
-    },
-    {
         "name": "Level Up",
         "short_name": "levelup",
         "url": "https://cactus-chewata.web.app/"
@@ -13,5 +8,10 @@ games = [
         "name":"Climate Game",
         "short_name": "climategame",
         "url": "https://climate-app-react.vercel.app/"
+    },
+    {
+        "name": "Match Africa",
+        "short_name": "matchafrica",
+        "url": "https://match-africa-host.vercel.app"
     }
 ]
