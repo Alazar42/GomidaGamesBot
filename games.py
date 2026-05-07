d48e1974261f9d3d012ceacc273dbccc4919c7b5
Alazar42 <alazartesfaye42@gmail.com>
2026-05-07 20:01:29 +0300

    update game list: replace existing games with Penalty Shootout Game and comment out others

--- games.py
+++ games.py
@@ -1,17 +1,22 @@
 games = [
+    # {
+    #     "name": "Level Up",
+    #     "short_name": "levelup",
+    #     "url": "https://cactus-chewata.web.app/"
+    # },
+    # {
+    #     "name":"Climate Game",
+    #     "short_name": "climategame",
+    #     "url": "https://climate-game-nu.vercel.app/"
+    # },
+    # {
+    #     "name": "Match Africa",
+    #     "short_name": "matchafrica",
+    #     "url": "https://match-africa-host.vercel.app/"
+    # }
     {
-        "name": "Level Up",
-        "short_name": "levelup",
-        "url": "https://cactus-chewata.web.app/"
-    },
-    {
-        "name":"Climate Game",
-        "short_name": "climategame",
-        "url": "https://climate-game-nu.vercel.app/"
-    },
-    {
-        "name": "Match Africa",
+        "name": "Penalty Shootout Game",
         "short_name": "matchafrica",
-        "url": "https://match-africa-host.vercel.app/"
+        "url": "https://penality-shootout-game-web-v65834.vercel.app/"
     }
 ]
